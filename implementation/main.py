111d174cc56a040f6ddd6df9b3104f3362910db0
Sunitha Selvan <sunitha98selvan@gmail.com>
2020-06-19 19:48:55 +0530

    Update main.py

--- implementation/main.py
+++ implementation/main.py
@@ -1,9 +1,14 @@
 from vertex import Vertex
 import random
 
+def generate_weights(n):
+    weights = [[random.randint(1,100) for i in range(n)] for j in range(n)]
+    return weights
+
 def main():
     nodes = 10
     vertices =[]
     for i in range(nodes):
         v_traffic = random.randint(1,50)
         vertices.append(Vertex(i,v_traffic)) 
+    weights = generate_weights(nodes)
